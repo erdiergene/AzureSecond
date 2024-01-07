fe1deff0b771c0c81c0c9ceebb6febf58237d98c
erdi ergene <erdieergene@gmail.com>
2024-01-07 22:14:50 +0300

    Initial Commit

---
+++ AzureSecond/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AzureSecond
+//
+//  Created by Erdi Ergene on 7.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
